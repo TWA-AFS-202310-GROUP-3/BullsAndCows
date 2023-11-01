a4f629768324bfb78c781109d991fdb83f90af3a
YChen110 <Ychen110@slb.com>
2023-11-01 17:55:18 +0800

    test: add test case for guess number digit and position partial correct

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -75,6 +75,25 @@ namespace BullsAndCowsTest
             Assert.Equal("2A0B", result);
         }
 
+        [Theory]
+        [InlineData("1562")]
+        public void Should_return_1A1B_when_guess_given_guess_digit_value_and_position_partial_correct(string guessNumber)
+        {
+            //Given
+            string secretNumber = "1234";
+
+            Mock<SecretGenerator> mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(secret => secret.GenerateSecret()).Returns(secretNumber);
+
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+
+            //When
+            string result = game.Guess(guessNumber);
+
+            //Then
+            Assert.Equal("1A1B", result);
+        }
+
         //Invalid length input
         //Invalid digit input
         //Duplicate digit input
