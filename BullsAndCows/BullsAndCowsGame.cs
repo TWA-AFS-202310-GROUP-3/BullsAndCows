c06b815a54cd0e8cb06beccf09a2b77348654443
YChen110 <Ychen110@slb.com>
2023-11-01 16:54:11 +0800

    feat: add guess logic for guess number all corrects

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -4,17 +4,24 @@ namespace BullsAndCows
 {
     public class BullsAndCowsGame
     {
+        private readonly string secretNumber;
         private readonly SecretGenerator secretGenerator;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            secretNumber = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            if (guess.Equals(secretNumber))
+            {
+                return "4A0B";
+            }
+
+            return string.Empty;
         }
     }
 }
