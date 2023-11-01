abce08f68ee42eaa1e5ca273215e585bb08c1f0d
YChen110 <Ychen110@slb.com>
2023-11-02 01:12:55 +0800

    feat: add random generate secretNumber logic

--- BullsAndCows/SecretGenerator.cs
+++ BullsAndCows/SecretGenerator.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BullsAndCows
 {
     public class SecretGenerator
     {
+
         public virtual string GenerateSecret()
         {
-            return string.Empty;
+            var random = new Random();
+            HashSet<int> numbers = new HashSet<int>();
+            while (numbers.Count < 4)
+            {
+                numbers.Add(random.Next(0, 9));
+            }
+
+            return string.Join(string.Empty, numbers.ToArray());
         }
     }
 }
