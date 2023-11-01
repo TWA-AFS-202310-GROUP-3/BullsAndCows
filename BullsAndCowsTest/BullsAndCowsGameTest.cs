de262820c793cc9418ff25c0a0c60dd83ab2cea2
Kailin Liu <KLiu13@slb.com>
2023-11-01 17:01:38 +0800

    test: add return 4A0B

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,17 +1,34 @@
 using BullsAndCows;
+using Moq;
+using System;
 using Xunit;
 
 namespace BullsAndCowsTest
 {
     public class BullsAndCowsGameTest
     {
+        //[Fact]
+        //public void Should_create_BullsAndCowsGame()
+        //{
+        //    var secretGenerator = new SecretGenerator();
+        //    var game = new BullsAndCowsGame(secretGenerator);
+        //    Assert.NotNull(game);
+        //    Assert.True(game.CanContinue);
+        //}
+
         [Fact]
-        public void Should_create_BullsAndCowsGame()
+        public void Should_return_4A0B_when_guess_given_guess_number_and_secret_are_same()
         {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
+            //Given
+            string guessNumber = "1234";
+            string secret = "1234";
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //When
+            string result = game.Guess(guessNumber);
+            //Then
+            Assert.Equal("4A0B", result);
         }
     }
 }
