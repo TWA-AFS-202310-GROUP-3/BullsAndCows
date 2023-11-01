b42c36385e20b2763fb99ce033e860d06081d782
Wang Jing <jwang315@slb.com>
2023-11-01 18:07:50 +0800

    test: add test return 0A4B given digital all correct but position all incorrect

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -96,7 +96,7 @@ namespace BullsAndCowsTest
         [InlineData("1324")]
         [InlineData("2134")]
         [InlineData("3214")]
-        public void Should_return_2A2B_when_guess_given_position_correct_and_digit_partial_correct(string guessNumber)
+        public void Should_return_2A2B_when_guess_given_digit_correct_but_positon_partial_correct(string guessNumber)
         {
             // Given
             string secret = "1234";
@@ -111,6 +111,28 @@ namespace BullsAndCowsTest
 
             // Then
             Assert.Equal("2A2B", result);
+        }
+
+        [Theory]
+        [InlineData("2341")]
+        [InlineData("3412")]
+        [InlineData("4123")]
+        [InlineData("2143")]
+        public void Should_return_0A4B_when_guess_given_values_correct_but_position_all_incorrect(string guessNumber)
+        {
+            // Given
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+
+            // When
+            string result = game.Guess(guessNumber);
+
+            // Then
+            Assert.Equal("0A4B", result);
         }
     }
 }
