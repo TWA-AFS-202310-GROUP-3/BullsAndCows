4a0f0cc4ebd3bd4fc2ca8920e18c71ad46ef2006
Kailin Liu <KLiu13@slb.com>
2023-11-01 17:45:16 +0800

    feat: complete add return 2A2B code logic already exist

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -95,5 +95,20 @@ namespace BullsAndCowsTest
             //Then
             Assert.Equal("0A4B", result);
         }
+
+        [Theory]
+        [InlineData("1243")]
+        public void Should_return_2A2B_when_guess_given_guess_number_have_four_correct_but_different_positions(string guessNumber)
+        {
+            //Given
+            string secret = "1234";
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //When
+            string result = game.Guess(guessNumber);
+            //Then
+            Assert.Equal("2A2B", result);
+        }
     }
 }
