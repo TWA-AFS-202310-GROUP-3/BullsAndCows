c68c59be3541d3f47817909dbcfd3cf66131d81e
Shuhan Jin <sjin9@slb.com>
2023-11-01 16:59:47 +0800

    test for 4A0B

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -12,6 +13,24 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_given_Guess_when_secret_and_guess_same()
+        {
+            //Given
+            string guess = "1234";
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(s => s.GenerateSecret()).Returns(secret);
+            //set up mockedSecretGenerator, return (secret) when use GenerateSecret() method
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //When
+            string result = game.Guess(guess);
+            //Then
+            Assert.Equal("4A0B", result);
         }
     }
 }
