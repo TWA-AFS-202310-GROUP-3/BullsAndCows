66eb824196472643ef2e05754534a34d0baee70a
YChen110 <Ychen110@slb.com>
2023-11-01 16:50:36 +0800

    test: add test for given guess all corrects

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -13,5 +14,33 @@ namespace BullsAndCowsTest
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
         }
+
+        [Fact]
+        public void Should_return_4A0B_when_guess_given_guess_all_corrects()
+        {
+            //Given
+            string secretNumber = "1234";
+            string guessNumber = "1234";
+
+            Mock<SecretGenerator> mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(secret => secret.GenerateSecret()).Returns(secretNumber);
+
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+
+            //When
+            string result = game.Guess(guessNumber);
+
+            //Then
+            Assert.Equal("4A0B", result);
+        }
+
+        //should return 4A0B when value all correct and in correction position
+        //should return 0A0B when value all wrong
+        //should return 0AyB when value have y correct digit but wrong position
+        //should return xA0B when value have x correct digit in correction position
+
+        //Invalid length input
+        //Invalid digit input
+        //Duplicate digit input
     }
 }
