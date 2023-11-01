59fc68053cbc3d2295f4970cb61b4a73ae1b5162
Shuhan Jin <sjin9@slb.com>
2023-11-01 17:56:53 +0800

    test: create test for 0A4B

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -89,5 +89,23 @@ namespace BullsAndCowsTest
             //Then
             Assert.Equal("0A0B", result);
         }
+
+        [Theory]
+        [InlineData("4321")]
+        public void Should_return_0A0B_given_Guess_when_all_digit_correct_and_position_incorrect(string guess)
+        {
+            //Given
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(s => s.GenerateSecret()).Returns(secret);
+            //set up mockedSecretGenerator, return (secret) when use GenerateSecret() method
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+            //When
+            string result = game.Guess(guess);
+            //Then
+            Assert.Equal("0A4B", result);
+        }
     }
 }
