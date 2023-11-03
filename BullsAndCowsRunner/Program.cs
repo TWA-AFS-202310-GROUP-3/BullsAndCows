6f63e46fa0db8e5d4ea0fd515c49bc15454b0d9f
Kailin Liu <KLiu13@slb.com>
2023-11-03 08:46:16 +0800

    feat: complete guess number game

--- BullsAndCowsRunner/Program.cs
+++ BullsAndCowsRunner/Program.cs
@@ -7,16 +7,42 @@ namespace BullsAndCowsRunner
     {
         public static void Main(string[] args)
         {
-            SecretGenerator secretGenerator = new SecretGenerator();
-            BullsAndCowsGame game = new BullsAndCowsGame(secretGenerator);
-            while (game.CanContinue)
+            //SecretGenerator secretGenerator = new SecretGenerator();
+            //BullsAndCowsGame game = new BullsAndCowsGame(secretGenerator);
+            //while (game.CanContinue)
+            //{
+            //    var input = Console.ReadLine();
+            //    var output = game.Guess(input);
+            //    Console.WriteLine(output);
+            //}
+
+            //Console.WriteLine("Game Over");
+
+            BullsAndCowsGame game = new BullsAndCowsGame();
+            Console.WriteLine("Welcome to Bulls and Cows game!");
+
+            for (int i = 0; i < 6; i++)
             {
-                var input = Console.ReadLine();
-                var output = game.Guess(input);
-                Console.WriteLine(output);
+                Console.WriteLine("Please enter your guess:");
+                string guess = Console.ReadLine();
+                string result = game.MakeGuess(guess);
+
+                if (result == "4A0B")
+                {
+                    Console.WriteLine("Congratulations! You guessed the right number!");
+                    Console.WriteLine("Secret number is: " + game.ReturnSecretNumber());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                    //Console.WriteLine("Your guesses so far:");
+                    Console.WriteLine(game.GetGuessHistory());
+                }
             }
 
-            Console.WriteLine("Game Over");
+            Console.WriteLine("Game over!");
+            Console.WriteLine("Secret number is: " + game.ReturnSecretNumber());
         }
     }
 }
